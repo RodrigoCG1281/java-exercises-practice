15b7e973d1d70ce90c1b7850c4b33fd84df63900
Rodriwo1281 <rodri153060@gmail.com>
2024-04-26 23:13:02 -0500

    POO en Java

--- screenmatch/src/Pelicula.java
+++ screenmatch/src/Pelicula.java
@@ -3,6 +3,25 @@ public class Pelicula {
     int fechaDeLanzamiento;
     int duracionEnMinutos;
     boolean incluidoEnElPlan;
+    double sumaDeEvaluaciones = 0;
+    int totalDeIteraciones = 0;
+
+
+    void fichaTecnica(){
+        System.out.println("El nombre de la pelicula es: " + nombre);
+        System.out.println("La fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("La duracion es de: " + duracionEnMinutos + " minutos");
+    }
+
+    void evalua(double nota){
+        sumaDeEvaluaciones = sumaDeEvaluaciones + nota;
+        totalDeIteraciones++;
+
+    }
+
+    void calcular_media(){
+        System.out.println("La media es: " + sumaDeEvaluaciones / totalDeIteraciones);
+    }
 
 
 
--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -4,21 +4,20 @@ public class Principal {
         miPelicula.nombre = "Encanto";
         miPelicula.fechaDeLanzamiento = 2021;
         miPelicula.duracionEnMinutos = 120;
+        miPelicula.fichaTecnica();
+        miPelicula.evalua(7);
+        miPelicula.evalua(8);
+        miPelicula.evalua(9);
+        miPelicula.evalua(10);
 
-        System.out.println("Mi pelicula es: " + miPelicula.nombre);
-        System.out.println("Fue lanzada en: "+ miPelicula.fechaDeLanzamiento);
-        System.out.println("Y tiene una duracion de: " + miPelicula.duracionEnMinutos);
+        System.out.println(miPelicula.sumaDeEvaluaciones);
+        miPelicula.calcular_media();
 
         Pelicula otraPelicula = new Pelicula();
         otraPelicula.nombre = "Matrix";
         otraPelicula.fechaDeLanzamiento = 2014;
         otraPelicula.duracionEnMinutos = 180;
-
-        System.out.println("Mi otra pelicula es: " + otraPelicula.nombre);
-        System.out.println("Fue lanzada en: "+ otraPelicula.fechaDeLanzamiento);
-        System.out.println("Y tiene una duracion de: " + otraPelicula.duracionEnMinutos);
-
-
-
+        //otraPelicula.fichaTecnica();
     }
+
 }
