6f0ad57ddd24846f374c5bb43ac8bbbe683afeb0
Rodriwo1281 <rodri153060@gmail.com>
2024-04-27 15:07:39 -0500

    getters y setters

--- screenmatch/src/Principal.java
+++ screenmatch/src/Principal.java
@@ -1,30 +1,27 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Encanto";
-        miPelicula.fechaDeLanzamiento = 2021;
-        miPelicula.duracionEnMinutos = 120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+
         miPelicula.fichaTecnica();
         miPelicula.evalua(7);
         miPelicula.evalua(8);
         miPelicula.evalua(9);
         miPelicula.evalua(10);
 
-//        System.out.println(miPelicula.sumaDeEvaluaciones); //detecta el error
+        System.out.println(miPelicula.getTotalDeLasIteraciones());
         miPelicula.calcular_media();
 
-        System.out.println("*****************");
 
-//        miPelicula.sumaDeEvaluaciones = 2; detecta el error
-//        miPelicula.totalDeIteraciones = 1;
-
-        miPelicula.calcular_media();
-
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.fechaDeLanzamiento = 2014;
-        otraPelicula.duracionEnMinutos = 180;
-        //otraPelicula.fichaTecnica();
+//        Pelicula otraPelicula = new Pelicula();
+//        otraPelicula.nombre = "Matrix";
+//        otraPelicula.fechaDeLanzamiento = 2014;
+//        otraPelicula.duracionEnMinutos = 180;
+//        //otraPelicula.fichaTecnica();
     }
 
 }
---
+++ screenmatch/src/com/aluracursos/screenmatch/modelos/Pelicula.java
@@ -0,0 +1,70 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Pelicula {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    //agrego private para que un tercero no pueda acceder a estas variables y de esa manera brindar seguridad
+    private double sumaDeEvaluaciones = 0;
+    private int totalDeIteraciones = 0;
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre; //this.nombre hace referencia al atributo de nuestra clase pelicula
+                              // nombre como tal hace referencia al valor que recibimos del metodo setNombre
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento) {
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalDeLasIteraciones (){
+        return totalDeIteraciones;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void fichaTecnica(){
+        System.out.println("El nombre de la pelicula es: " + nombre);
+        System.out.println("La fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("La duracion es de: " + duracionEnMinutos + " minutos");
+    }
+
+    public void evalua(double nota){
+        sumaDeEvaluaciones = sumaDeEvaluaciones + nota;
+        totalDeIteraciones++;
+
+    }
+
+    public void calcular_media(){
+        System.out.println("La media es: " + sumaDeEvaluaciones / totalDeIteraciones);
+    }
+    //Hago uso de void cuando no retorno un valor que usare en mi clase principal
+    //Y hago uso de otro tipo cuando si usare ese valor despues
+
+
+
+
+}
